f4643441be70ec8826f6191514149dc2d3711203
MB <milthonbor@gmail.com>
2021-10-25 17:58:05 -0700

    clases

---
+++ 01-dart-basico/clases.dart
@@ -0,0 +1,26 @@
+void main(){
+  
+  //final = no se pude cambiar nunca, solo sus propidades
+  //final es parecido al const 
+  final wolverin = new Heroe(nombre:'Logan', poder:'Regeneración');// new ya no es necesario usar
+  
+  print(wolverin);
+  
+}
+
+
+class Heroe{
+  
+  String nombre='';
+  String poder='';
+  
+  Heroe({String nombre='Sin nombre',String poder='Sin poder'}){
+    this.nombre = nombre;
+    this.poder = poder;
+  }
+  
+  String toString(){
+    return 'Nombre: ${this.nombre} - Poder: ${this.poder}';
+  }
+
+}
