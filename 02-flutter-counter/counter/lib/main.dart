8ec71d6dfb0c652dce8ba325d068f14a1709c82e
MB <milthonbor@gmail.com>
2021-10-26 13:37:50 -0700

    limpiza

--- 02-flutter-counter/counter/lib/main.dart
+++ 02-flutter-counter/counter/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Primer app'),
     );
   }
 }
@@ -94,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Contador',
             ),
             Text(
               '$_counter',
