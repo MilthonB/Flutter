be5d3335bd37112ab5ca336611f1d7cc9f1f2cd7
MB <milthonbor@gmail.com>
2021-10-25 18:29:10 -0700

    getter y setter

---
+++ 01-dart-basico/getter-setter.dart
@@ -0,0 +1,37 @@
+
+void main(){
+  
+  
+  final cuadrado = new Cuadrado();
+  
+  cuadrado.lados = 10;
+  print(cuadrado.area);
+  
+  
+  
+}
+
+
+
+class Cuadrado{
+  
+  //Propiedades privadas _
+  double _lado=0.0;
+  double _area=0.0;
+  
+  
+  set lados(double valor){
+    if(valor <= 0){
+      throw('El lado no puede ser menor o igual a 0');
+    }
+    
+    
+    _lado = valor;
+  }
+  
+ 
+  
+  double get area{
+    return  _lado * _lado;
+  }
+}
