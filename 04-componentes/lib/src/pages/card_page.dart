f9b1310b43828c121f50afc601244aad6bdd461c
MB <milthonbor@gmail.com>
2021-10-26 20:14:11 -0700

    creacion de cards

--- 04-componentes/lib/src/pages/card_page.dart
+++ 04-componentes/lib/src/pages/card_page.dart
@@ -9,6 +9,32 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: _crearCards(),
+    );
+  }
+
+  Widget _crearCards() {
+    return Card(
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo,
+              color: Colors.blue,
+            ),
+            title: Text('Hola soy el titulo de la card'),
+            subtitle: Text(
+                'Hola yo soy el sub del titulo agregado anteriormente, puedes continuar usando la app y jugar con los componentes'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: () {}, child: Text('Cancelar')),
+              TextButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
