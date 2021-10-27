3d586ab45e1c9685f8d2c7467d7d6a955286d336
MB <milthonbor@gmail.com>
2021-10-26 17:40:28 -0700

    home page

--- 04-componentes/lib/main.dart
+++ 04-componentes/lib/main.dart
@@ -1,36 +1,49 @@
 import 'package:flutter/material.dart';
 
 //Importaciones propias de la app
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/home_page.dart';
+// import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Componentes'),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text('ListTile title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('ListTile title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('ListTile title'),
-            )
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
+
+  // List<Widget> _crearItems() {
+  //   List<Widget> lista = [];
+
+  //   for (String opt in opciones) {
+  //     final tempWidget = ListTile(
+  //       title: Text(opt),
+  //     );
+  //     lista..add(tempWidget)..add(Divider());
+  //   }
+
+  //   return lista;
+  // }
+
+  // List<Widget> _crearItemsCortos() {
+  //   return opciones.map((item) {
+  //     return Column(
+  //       children: [
+  //         ListTile(
+  //           title: Text(item + '!'),
+  //           subtitle: Text('Cualquier cosa'),
+  //           leading: Icon(Icons.subdirectory_arrow_right_outlined),
+  //           trailing: Icon(Icons.architecture_sharp),
+  //           onTap: () {},
+  //         ),
+  //         Divider(),
+  //       ],
+  //     );
+  //   }).toList();
+  // }
 }
