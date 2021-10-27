1cf0edd5fd3793e9cea705dd0786f0574a1fd47e
MB <milthonbor@gmail.com>
2021-10-27 15:52:13 -0700

    inputs

--- 04-componentes/lib/src/routes/routes.dart
+++ 04-componentes/lib/src/routes/routes.dart
@@ -1,10 +1,11 @@
-import 'package:componentes/src/pages/animated_container.dart';
-import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getRoutAplication() {
   return {
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> getRoutAplication() {
     'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
     'card': (BuildContext context) => const CardPage(),
-    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage()
   };
 }
