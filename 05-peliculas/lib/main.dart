6edd510a50f0a74402cfffeeb439fee656dc363b
MB <milthonbor@gmail.com>
2021-10-28 14:45:42 -0700

    card widget swiper

--- 05-peliculas/lib/main.dart
+++ 05-peliculas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas/screens/details_screen.dart';
-import 'package:peliculas/screens/home_screen.dart';
+import 'package:peliculas/screens/screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,12 +7,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Películas App',
-        initialRoute: 'home',
-        routes: {
-          'home': (_) => HomeScreen(),
-          'details': (_) => DetailsScreen()
-        });
+      debugShowCheckedModeBanner: false,
+      title: 'Películas App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
+      },
+      theme: ThemeData.dark(),
+      // theme: ThemeData.light().copyWith(
+      //   appBarTheme: AppBarTheme(
+      //     color: Colors.indigo
+      //   )
+      // ),
+    );
   }
 }
--- 05-peliculas/lib/screens/home_screen.dart
+++ 05-peliculas/lib/screens/home_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:peliculas/widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Películas en cines'),
+        ),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search_outlined),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          // TODO: CardSwiper
+          CardSwiper()
+          //Listado horizontal de películas
+        ],
       ),
     );
   }
