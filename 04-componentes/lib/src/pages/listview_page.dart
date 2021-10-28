47fc8cc642c7a7497834f609b3ca34f55133efea
MB <milthonbor@gmail.com>
2021-10-27 18:34:32 -0700

    listview implement

--- 04-componentes/lib/src/pages/listview_page.dart
+++ 04-componentes/lib/src/pages/listview_page.dart
@@ -8,13 +8,28 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
+  List<int> _listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: Text('Listas...'),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        final numero = _listaNumeros[index];
+        return FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image:
+                NetworkImage('https://picsum.photos/500/300?random=$numero'));
+      },
     );
   }
 }
