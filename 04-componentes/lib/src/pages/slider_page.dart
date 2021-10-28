f27be7ec6ac93309be0442731a358da2029d580f
MB <milthonbor@gmail.com>
2021-10-27 17:53:00 -0700

    slider

--- 04-componentes/lib/src/pages/slider_page.dart
+++ 04-componentes/lib/src/pages/slider_page.dart
@@ -8,6 +8,7 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +16,41 @@ class _SliderPageState extends State<SliderPage> {
         title: Text('Slider'),
       ),
       body: Container(
-        child: Text('Slider Page'),
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: [
+            _crearSlider(),
+            Expanded(
+              child: _crearImagen(),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      value: _valorSlider,
+      label: 'Tamaño de la imagen',
+      divisions: 20,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor) {
+        setState(() {
+          _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  _crearImagen() {
+    return Image(
+      width: _valorSlider,
+      fit: BoxFit.contain,
+      image: NetworkImage(
+          'https://www.pinpng.com/pngs/m/157-1572510_la-expresin-que-todos-queremos-ver-de-goku.png'),
     );
   }
 }
