893cc08eb719ff26b7783f16bd52008bee8d7382
MB <milthonbor@gmail.com>
2021-10-27 19:09:14 -0700

    loading scroll

--- 04-componentes/lib/src/pages/listview_page.dart
+++ 04-componentes/lib/src/pages/listview_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListaPage extends StatefulWidget {
@@ -8,7 +10,24 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> _listaNumeros = [];
+
+  ScrollController _scroll = new ScrollController();
+  int _ultimovalor = 0;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _cargar10();
+
+    _scroll.addListener(() {
+      if (_scroll.position.pixels == _scroll.position.maxScrollExtent) {
+        // _cargar10();
+        _fetchData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +35,18 @@ class _ListaPageState extends State<ListaPage> {
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: [
+          _crearLista(),
+          _crearLoding(),
+        ],
+      ),
     );
   }
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scroll,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         final numero = _listaNumeros[index];
@@ -31,5 +56,55 @@ class _ListaPageState extends State<ListaPage> {
                 NetworkImage('https://picsum.photos/500/300?random=$numero'));
       },
     );
+  }
+
+  void _cargar10() {
+    for (var i = 0; i < 10; i++) {
+      _ultimovalor++;
+      _listaNumeros.add(_ultimovalor);
+    }
+
+    setState(() {});
+  }
+
+  Future _fetchData() async {
+    isLoading = true;
+    setState(() {});
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, httpResponse);
+  }
+
+  httpResponse() {
+    isLoading = false;
+
+    _scroll.animateTo(
+      _scroll.position.pixels + 100,
+      duration: new Duration(milliseconds: 250),
+      curve: Curves.fastOutSlowIn,
+    );
+    _cargar10();
+  }
+
+  Widget _crearLoding() {
+    if (isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(
+            height: 30.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
