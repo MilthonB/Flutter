1a018bf34ed5aa85a4d18d99fd0e11bf7f98fbe0
MB <milthonbor@gmail.com>
2021-10-28 14:31:33 -0700

    home datail screen

--- 05-peliculas/lib/main.dart
+++ 05-peliculas/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/screens/details_screen.dart';
+import 'package:peliculas/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Películas App',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'details': (_) => DetailsScreen()
+        });
   }
 }
