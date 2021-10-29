378090b69a6a566e47ae3628d0cf6c2ce6411f84
MB <milthonbor@gmail.com>
2021-10-28 19:38:44 -0700

    detail screen

--- 05-peliculas/lib/screens/details_screen.dart
+++ 05-peliculas/lib/screens/details_screen.dart
@@ -3,11 +3,108 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //TODO: Cambiar luego por una instancia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+        body: CustomScrollView(
+      slivers: [
+        _CustomeAppbar(),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          _PosterAndTitle(),
+          // Text('aaa')
+        ]))
+      ],
+    ));
+  }
+}
+
+class _CustomeAppbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.green,
+      expandedHeight: 200,
+      floating: false,
+      pinned: false,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          color: Colors.black12,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            'Movie.title',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
         ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage(
+                'https://via.placeholder.com/200x300',
+              ),
+              height: 150,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'Movie.originaltitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    'movie.voteAvarage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
